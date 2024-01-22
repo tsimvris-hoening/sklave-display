b5d2aca51730597ee83bd88d98924635714cd97d
tsimvris-hoening <pt@hoening.de>
2024-01-22 13:43:51 +0100

    working prototyp

--- script.js
+++ script.js
@@ -1,13 +1,58 @@
-const images = [
+const imagesHorizontal = [
+    'https://hoening.de/de/dashboard/shots/screenshot-0.png',
     'https://hoening.de/de/dashboard/shots/screenshot-1.png',
-    'https://hoening.de/de/dashboard/shots/screenshot-2.png',
-    'https://hoening.de/de/dashboard/shots/screenshot-3.png'
+    'https://hoening.de/de/dashboard/shots/screenshot-2.png'
 ];
+const imagesVertikal = [
+    'https://hoening.de/de/dashboard/shots/vertikal-screenshot-0.png',
+    'https://hoening.de/de/dashboard/shots/vertikal-screenshot-1.png',
+    'https://hoening.de/de/dashboard/shots/vertikal-screenshot-2.png'
+];
+let cacheBuster = "?t=" + new Date().getTime(); // Create a unique timestamp
 
-let currentIndex = 0;
-
-setInterval(() => {
-    currentIndex = (currentIndex + 1) % images.length; // Cycle through the images
+document.addEventListener('DOMContentLoaded', (event) => {
+    let width = window.innerWidth;
+    let height = window.innerHeight;
     const imageElement = document.getElementById('slider-image');
-    imageElement.src = images[currentIndex];
-}, 15000); // Change every 15 seconds
+    if (width > height) {
+        imageElement.src = imagesHorizontal[0] + cacheBuster;
+    } else {
+        imageElement.src = imagesVertikal[0] + cacheBuster;
+    }
+    window.addEventListener('resize', () => {
+        width = window.innerWidth;
+        height = window.innerHeight;
+    });
+    let currentIndexHorizontal = 0;
+    let currentIndexVertikal = 0;
+    if (!imageElement) {
+        console.error('Slider image element not found!');
+        return;
+    }
+    let autoChangeInterval;
+    const intervalDuration = 15000;
+    function showNextImage() {
+        let cacheBuster = "?t=" + new Date().getTime();
+
+        if (width > height) {
+            currentIndexHorizontal = (currentIndexHorizontal + 1) % imagesHorizontal.length;
+            imageElement.src = imagesHorizontal[currentIndexHorizontal] + cacheBuster;
+        } else {
+            currentIndexVertikal = (currentIndexVertikal + 1) % imagesVertikal.length;
+            imageElement.src = imagesVertikal[currentIndexVertikal] + cacheBuster;
+        }
+    }
+   function startOrResetInterval() {
+        if (autoChangeInterval) {
+            clearInterval(autoChangeInterval);
+        }
+
+        autoChangeInterval = setInterval(showNextImage, intervalDuration);
+    }
+    startOrResetInterval();
+
+    document.body.addEventListener('click', () => {
+        showNextImage();
+        startOrResetInterval()
+    });
+});
